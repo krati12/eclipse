d0f5765fcd6db26fbb49875d88b1ac16d36d5ebd
krati12 <rockingsherya@gmail.com>
2018-02-15 15:42:45 -0500

    3:42 15-feb

---
+++ workspace/MFS/src/main/java/org/krati/java/MFS/App.java
@@ -0,0 +1,13 @@
+package org.krati.java.MFS;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
